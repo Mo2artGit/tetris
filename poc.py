1cda5ea26374979c5bab8c4962bb7f11cfa23a88
Mo2artGit <tuj76813@temple.edu>
2022-09-19 22:22:13 -0400

    Create poc.py

---
+++ poc.py
@@ -0,0 +1,88 @@
+
+# import pygame module in this program 
+import pygame
+  
+# activate the pygame library .  
+# initiate pygame and give permission  
+# to use pygame's functionality.  
+pygame.init()
+  
+# create the display surface object  
+# of specific dimension..e(500, 500).  
+win = pygame.display.set_mode((500, 500))
+  
+# set the pygame window name 
+pygame.display.set_caption("Moving rectangle")
+  
+# object current co-ordinates 
+x = 200
+y = 0
+  
+# dimensions of the object 
+width = 60
+height = 20
+  
+# velocity / speed of movement
+vel = 5
+  
+# Indicates pygame is running
+run = True
+  
+# infinite loop 
+while run:
+    # creates time delay of 30ms 
+    pygame.time.delay(30)
+    if y<500-height:
+        y+=1
+
+    # iterate over the list of Event objects  
+    # that was returned by pygame.event.get() method.  
+    for event in pygame.event.get():
+
+          
+        # if event object type is QUIT  
+        # then quitting the pygame  
+        # and program both.  
+        if event.type == pygame.QUIT:
+              
+            # it will make exit the while loop 
+            run = False
+    # stores keys pressed 
+    keys = pygame.key.get_pressed()
+      
+    # if left arrow key is pressed
+    if keys[pygame.K_LEFT] and x>0:
+          
+        # decrement in x co-ordinate
+        x -= vel
+          
+    # if right arrow key is pressed
+    if keys[pygame.K_RIGHT] and x<500-width:
+          
+        # increment in x co-ordinate
+        x += vel
+         
+    # if up arrow key is pressed   
+    if keys[pygame.K_UP] and y>0:
+          
+        # down
+        y=500-height
+          
+    # if down arrow key is pressed   
+    if keys[pygame.K_DOWN] and y<500-height:
+        # increment in y co-ordinate
+        y += vel
+         
+              
+    # completely fill the surface object  
+    # with black colour  
+    win.fill((0, 0, 0))
+      
+    # drawing object on screen which is rectangle here 
+    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
+      
+    # it refreshes the window
+    pygame.display.update() 
+  
+# closes the pygame window 
+pygame.quit()
